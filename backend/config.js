4927154247d54a136ab052b186aa01b028b9f3e2
farrrahhh <18222096@std.stei.itb.ac.id>
2024-11-14 12:49:49 +0700

    fix: import dotenv in config.js and remove unnecessary newline in index.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
-
+import dotenv from "dotenv";
+dotenv.config();
 const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
   host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT,
--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,6 @@ const SECRET_KEY = process.env.SECRET_KEY; // Load the secret key from environme
 
 // Middleware
 app.use(cors()); // Enable CORS for all origins
-
 app.use(express.json()); // Parse JSON request bodies
 
 // Middleware to verify JWT
