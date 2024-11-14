f0b515b4da297b472d0ff209861f0e152c882e7b
farrrahhh <18222096@std.stei.itb.ac.id>
2024-11-14 16:51:14 +0700

    feat: add mysql2 dependency for database connection in config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import mysql2 from "mysql2";
 
 dotenv.config();
 const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
