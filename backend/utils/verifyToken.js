039662722b08de71a205fb70510346a3615e14bb
farrrahhh <18222096@std.stei.itb.ac.id>
2024-11-14 17:32:25 +0700

    feat: load environment variables using dotenv in verifyToken.js

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -1,5 +1,7 @@
 //backend/utils/verifyToken.js
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"];
