580185c373dc1c17fb9bdb174b2c8666654b0e52
farrrahhh <18222096@std.stei.itb.ac.id>
2024-11-14 15:41:51 +0700

    Revert "feat: add mysql2 dependency for database connection in config.js"
    This reverts commit c536afc12c9eab90d8a4376041a994e3ea832e25.

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,5 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-import mysql2 from "mysql2";
 
 dotenv.config();
 const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
