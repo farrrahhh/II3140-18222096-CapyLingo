954b71fe9776ca7dcc21b9fe32cf279f4725d002
farrrahhh <18222096@std.stei.itb.ac.id>
2024-11-14 12:45:44 +0700

    fix: change Sequelize dialect to "mysql" and specify dialectModule as "mysql2"

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,8 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
   host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT,
-  dialect: "mysql2",
+  dialect: "mysql",
+  dialectModule: require("mysql2"),
   logging: false,
 });
 
