1b0034d13f64659c277c3c1f1931c69bfc024f54
farrrahhh <18222096@std.stei.itb.ac.id>
2024-11-14 15:45:49 +0700

    refactor: remove backend server and related functionality

--- backend/routes/quizRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { submitQuiz, getQuizzes } from "../controllers/quizController.js";
-import { verifyToken } from "../middlewares/authMiddleware.js";
-
-const router = express.Router();
-router.post("/submit-quiz", verifyToken, submitQuiz);
-router.get("/quizzes", verifyToken, getQuizzes);
-
-export default router;
